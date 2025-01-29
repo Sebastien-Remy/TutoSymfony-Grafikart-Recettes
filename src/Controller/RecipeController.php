17cc2faea9be91effc3a557922689813fe781be0
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2025-01-29 12:01:30 +0100

    forms

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,17 +15,48 @@ final class RecipeController extends AbstractController
 {
 
     #[Route('/recette', name: 'recipe.index')]
-    public function index(): Response
+    public function index(RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig', []);
+        $shorts = $repository->findWithDurationLowerThan(10);
+        $recipes = $repository->findAll();
+        return $this->render('recipe/index.html.twig', [
+            'shorts' => $shorts,
+            'recipes' => $recipes,
+        ]);
     }
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9-_]+' ,'id' => '\d+'])]
-    public function show(Request $request, $slug, $id): Response
+    public function show($slug, $id, RecipeRepository $repository): Response
     {
+        $recipe = $repository->find($id);
+        // bad slug
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', [
+                'slug' => $recipe->getSlug(),
+                'id' => $recipe->getId(),
+                ], 301);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
+            'recipe' => $recipe,
+            ]
+        );
+    }
+
+    #[Route('/recette/{id}/edit', name: 'recipe.edit', requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'La recette à bien été modifiée');
+            return $this->redirectToRoute('recipe.index');
+        }
+        return $this->render('recipe/edit.html.twig', [
+                'recipe' => $recipe,
+                'form' => $form,
             ]
         );
     }
 }
+
